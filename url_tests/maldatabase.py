2513ca08a909bca1eec898bd0711ce1da115f96e
surya4682 <vsuryasagar96@gmail.com>
2023-03-01 13:31:33 -0500

    opening the textf file from the url_tests

--- url_tests/maldatabase.py
+++ url_tests/maldatabase.py
@@ -1,5 +1,5 @@
 def is_maldatabase(url):
-    with open('E:/NEU - MSCY/Capstone Project - CY7900 35732/Capstone/Backend_FastAPI/url_tests/urlhaus.abuse.ch.txt') as f:
+    with open('url_tests/urlhaus.abuse.ch.txt') as f:
         urls = [line.strip() for line in f]
     if url in urls:
         flag = 1
@@ -7,7 +7,7 @@ def is_maldatabase(url):
         flag = 2
 
 
-    with open('E:/NEU - MSCY/Capstone Project - CY7900 35732/Capstone/Backend_FastAPI/url_tests/delisted.txt') as f:
+    with open('url_tests/delisted.txt') as f:
         domains = [line.strip() for line in f]
     url_to_check = url.replace('https://', '').replace('http://', '').strip()  
     if url_to_check in domains:
